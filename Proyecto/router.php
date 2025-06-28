5cbc713b7ba4fdd0b17be97d4fbe5c1f59b583e5
JosueMejia20 <josuedanielm21@gmail.com>
2025-06-27 18:55:06 -0600

    Intento 1 arreglo archivo router

--- Proyecto/router.php
+++ Proyecto/router.php
@@ -8,6 +8,8 @@ $router->addRoute("GET", "/", "Controllers", "landing");
 $router->addRoute("GET", "/formulario", "Controllers", "formulario");
 $router->addRoute("GET", "/revisor/solicitud", "Controllers", "revisores");
 
+// Normalizar URI quitando el prefijo /Proyecto
+$basePath = "/Proyecto";
+$uri = str_replace($basePath, "", $_SERVER["REQUEST_URI"]);
 
-$router->dispatch($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
-?>
+$router->dispatch($_SERVER["REQUEST_METHOD"], $uri);
