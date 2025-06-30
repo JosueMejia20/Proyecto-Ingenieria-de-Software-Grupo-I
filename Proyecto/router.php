087df8d4d31402a3e007d01ed220347e9702ea29
JosueMejia20 <josuedanielm21@gmail.com>
2025-06-30 07:12:40 -0600

    Veremos

--- Proyecto/router.php
+++ Proyecto/router.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../app/controllers/HomeController.php';
+require_once __DIR__ . '/app/controllers/HomeController.php';
 require_once __DIR__ . '/../app/controllers/AdmisionesController.php';
 
 $routes = [
